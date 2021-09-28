54ecf86d80850eb2b4b3db189c2302d467388531
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-09-28 17:01:26 +0200

    Update AbJambotron.jsx

--- m_2_d_2app/src/components/AbJambotron.jsx
+++ m_2_d_2app/src/components/AbJambotron.jsx
@@ -1,8 +1,8 @@
 import { Jumbotron, Container } from 'react-bootstrap';
 const AbJambo = () => (
 	<Jumbotron fluid>
-		<Container class="bg-dark">
-			<h1>Welcome to Book World!!</h1>
+		<Container class="pl-5 pr-5">
+			<h1>Welcome to Book World!</h1>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
 				similique iusto maiores asperiores beatae eum quidem cum accusantium
