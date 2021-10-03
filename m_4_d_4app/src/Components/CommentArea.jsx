0319ceb7a91194d6308365ef44859ab6dc0dc8ed
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-03 17:38:36 +0200

    day 4

---
+++ m_4_d_4app/src/Components/CommentArea.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+export default class ComentArea extends React.Component {
+	render() {
+				return; <Form.Label>Example textarea</Form.Label>;
+				
+        //(
+			// <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+				{/* <Form.Control as="textarea" rows={3} />
+			</Form.Group> */}
+		//);
+	}
+}
--- m_4_d_4app/src/Components/SingleBook.jsx
+++ m_4_d_4app/src/Components/SingleBook.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import CommentArea from '../Components/CommentArea.jsx'
 
 export default class SingleBook extends React.Component {
 	state = { selected: false };
 	render() {
 		return (
-			<Card
-				style={{ border: this.state.selected ? '3px solid red' : '1px solid' }}
-				onClick={() => this.setState({ selected: !this.state.selected })}
-			>
-				<Card.Img variant="top" src={this.props.book.img} />
-				<Card.Body>
-					<Card.Title>{this.props.book.title}</Card.Title>
-				</Card.Body>
-			</Card>
+			<>
+				<Card
+					style={{
+						border: this.state.selected ? '3px solid red' : '1px solid',
+					}}
+					onClick={() => this.setState({ selected: !this.state.selected })}
+				>
+					<Card.Img variant="top" src={this.props.book.img} />
+					<Card.Body>
+						<Card.Title>{this.props.book.title}</Card.Title>
+					</Card.Body>
+				</Card>
+				{this.state.selcted && <CommentArea />}
+			</>
 		);
 	}
 }
