6e822c1051ecd4a2dca2dcc8ee53c8672fc4cbdf
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-03 15:54:29 +0200

    new

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './Components/NavBar';
+import Home from './Components/Carousels ';
 
 export default function App() {
 	return (
 		<>
 			<NavBar />
+			<Home />
 		</>
 	);
 }
